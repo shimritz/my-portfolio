c50512a659e09fff4c736cd7dbd35bcfd9061957
Shimrit Ziskand <shimrit021@gmail.com>
2023-07-26 14:43:55 +0300

    header component completed

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BiLogoDevTo } from "react-icons/bi";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https:://linkdin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https:://github.com" target="_blank">
+        {" "}
+        <FaGithub />
+      </a>
+
+      <a href="https:://facebook.com" target="_blank">
+        <BiLogoDevTo />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
