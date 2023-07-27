2425fc293c1bd2b7b7e7d05b72b745e8f565e240
Shimrit Ziskand <shimrit021@gmail.com>
2023-07-27 16:29:23 +0300

    services component completed

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,183 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* second service  */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* thirs service */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Teaching</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolorem sapiente deserunt placeat nostrum? Labore asperiores
+                porro odio eum deleniti nobis? Autem provident alias pariatur
+                quod esse, voluptate commodi officiis totam!
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
